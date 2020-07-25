241d7c24f7fc9884ed2ffc14278342733b3c0fcc
ankan saha <ankan@truckhall.com>
2020-07-26 04:15:14 +0530

    halfway done

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: bundle.js
-    },
+    // output: {
+    //     path: path.join(__dirname, '/dist'),
+    //     filename: bundle.js
+    // },
     module: {
         rules: [
             {
