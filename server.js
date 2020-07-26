6563d76a7ad612972923fdb90212767c81431e34
ankan saha <ankan@truckhall.com>
2020-07-26 05:37:20 +0530

    almost done

--- server.js
+++ server.js
@@ -7,4 +7,9 @@ const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
 
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static('dist'))
+}
+
 server.listen(port);
