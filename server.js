011caf21a5d401b63c19ebaf154ddcaff5269bd2
ankan saha <ankan@truckhall.com>
2020-07-26 12:00:42 +0530

    almost done

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+var express =require('express')
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+if(process.env.NODE_ENV === "production"){
+    server.use(express.static('dist'))
+}
+
 server.use(middlewares);
 server.use(router);
 
-
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('dist'))
-}
 
 server.listen(port);
